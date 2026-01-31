95d4d41652d461aaf5de3487b9913c1ed1efd9e0
Vidmantas <vidmantas@kitoks.lt>
2026-01-31 13:05:43 +0200

    First commit

---
+++ web/js/list_map.js
@@ -0,0 +1,195 @@
+"use strict";
+var markerIcon = {
+    anchor: new google.maps.Point(22, 16),
+    url: '/web/images/marker.png',
+}
+
+function mainMap(locationsMap) {
+    function locationData(locationURL, locationCategory, locationImg, locationTitle, locationAddress, locationPhone) {
+        return ('<div class="map-popup-wrap"><div class="map-popup"><div class="infoBox-close"><i class="fa fa-times"></i></div><div class="map-popup-category">' + locationCategory + '</div><a href="' + locationURL + '" class="listing-img-content fl-wrap"><img src="' + locationImg + '" alt=""></a> <div class="listing-content fl-wrap"><div class="listing-title fl-wrap"><h4><a href=' + locationURL + '>' + locationTitle + '</a></h4><span class="map-popup-location-info"><i class="fa fa-map-marker"></i>' + locationAddress + '</span><span class="map-popup-location-phone"><i class="fa fa-phone"></i>' + locationPhone + '</span></div></div></div></div>')
+    }
+
+    var locations = locationsMap.map(function(loc) {
+        return [
+            locationData(
+                '/barai/rodyti/' + loc.id + '.html',
+                '',
+                '/paveikslelis-' + loc.gallery_id_photo + '-crop400x265.jpg',
+                loc.name,
+                loc.address + ', ' + loc.city,
+                loc.phone
+            ),
+            loc.coordinates_lat,
+            loc.coordinates_lng,
+            0,
+            markerIcon
+        ];
+    });
+
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < locationsMap.length; i++) {
+        bounds.extend(new google.maps.LatLng({lat: parseFloat(locationsMap[i].coordinates_lat), lng: parseFloat(locationsMap[i].coordinates_lng)}));
+    }
+
+    var map = new google.maps.Map(document.getElementById('map-main'), {
+        zoom: 12,
+        scrollwheel: false,
+        center: bounds.getCenter(),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        zoomControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        panControl: false,
+        fullscreenControl: true,
+        navigationControl: false,
+        streetViewControl: false,
+        animation: google.maps.Animation.BOUNCE,
+        gestureHandling: 'cooperative',
+        styles: [{
+            "featureType": "administrative",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "color": "#444444"
+            }]
+        }]
+    });
+
+    map.fitBounds(bounds);
+
+
+    var boxText = document.createElement("div");
+    boxText.className = 'map-box'
+    var currentInfobox;
+    var boxOptions = {
+        content: boxText,
+        disableAutoPan: true,
+        alignBottom: true,
+        maxWidth: 0,
+        pixelOffset: new google.maps.Size(-145, -45),
+        zIndex: null,
+        boxStyle: {
+            width: "260px"
+        },
+        closeBoxMargin: "0",
+        closeBoxURL: "",
+        infoBoxClearance: new google.maps.Size(1, 1),
+        isHidden: false,
+        pane: "floatPane",
+        enableEventPropagation: false,
+    };
+    var markerCluster, marker, i;
+    var allMarkers = [];
+    var clusterStyles = [{
+        textColor: 'white',
+        url: '',
+        height: 50,
+        width: 50
+    }];
+
+
+    for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            icon: locations[i][4],
+            id: i
+        });
+        allMarkers.push(marker);
+        var ib = new InfoBox();
+        google.maps.event.addListener(ib, "domready", function () {
+            cardRaining()
+        });
+        google.maps.event.addListener(marker, 'click', (function (marker, i) {
+            return function () {
+                ib.setOptions(boxOptions);
+                boxText.innerHTML = locations[i][0];
+                ib.close();
+                ib.open(map, marker);
+                currentInfobox = marker.id;
+                var latLng = new google.maps.LatLng(locations[i][1], locations[i][2]);
+                map.panTo(latLng);
+                map.panBy(0, -180);
+                google.maps.event.addListener(ib, 'domready', function () {
+                    $('.infoBox-close').click(function (e) {
+                        e.preventDefault();
+                        ib.close();
+                    });
+                });
+            }
+        })(marker, i));
+    }
+    var options = {
+        imagePath: 'images/',
+        styles: clusterStyles,
+        minClusterSize: 2
+    };
+    markerCluster = new MarkerClusterer(map, allMarkers, options);
+    google.maps.event.addDomListener(window, "resize", function () {
+        var center = map.getCenter();
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center);
+    });
+
+    $('.nextmap-nav').click(function (e) {
+        e.preventDefault();
+        map.setZoom(15);
+        var index = currentInfobox;
+        if (index + 1 < allMarkers.length) {
+            google.maps.event.trigger(allMarkers[index + 1], 'click');
+        } else {
+            google.maps.event.trigger(allMarkers[0], 'click');
+        }
+    });
+    $('.prevmap-nav').click(function (e) {
+        e.preventDefault();
+        map.setZoom(15);
+        if (typeof (currentInfobox) == "undefined") {
+            google.maps.event.trigger(allMarkers[allMarkers.length - 1], 'click');
+        } else {
+            var index = currentInfobox;
+            if (index - 1 < 0) {
+                google.maps.event.trigger(allMarkers[allMarkers.length - 1], 'click');
+            } else {
+                google.maps.event.trigger(allMarkers[index - 1], 'click');
+            }
+        }
+    });
+    $('.map-item').click(function (e) {
+        e.preventDefault();
+        map.setZoom(15);
+        var index = currentInfobox;
+        var marker_index = parseInt($(this).attr('href').split('#')[1], 10);
+        google.maps.event.trigger(allMarkers[marker_index], "click");
+        if ($(this).hasClass("scroll-top-map")){
+            $('html, body').animate({
+                scrollTop: $(".map-container").offset().top+ "-80px"
+            }, 500)
+        }
+        else if ($(window).width()<1064){
+            $('html, body').animate({
+                scrollTop: $(".map-container").offset().top+ "-80px"
+            }, 500)
+        }
+    });
+    var zoomControlDiv = document.createElement('div');
+    var zoomControl = new ZoomControl(zoomControlDiv, map);
+
+    function ZoomControl(controlDiv, map) {
+        zoomControlDiv.index = 1;
+        map.controls[google.maps.ControlPosition.RIGHT_CENTER].push(zoomControlDiv);
+        controlDiv.style.padding = '5px';
+        var controlWrapper = document.createElement('div');
+        controlDiv.appendChild(controlWrapper);
+        var zoomInButton = document.createElement('div');
+        zoomInButton.className = "mapzoom-in";
+        controlWrapper.appendChild(zoomInButton);
+        var zoomOutButton = document.createElement('div');
+        zoomOutButton.className = "mapzoom-out";
+        controlWrapper.appendChild(zoomOutButton);
+        google.maps.event.addDomListener(zoomInButton, 'click', function () {
+            map.setZoom(map.getZoom() + 1);
+        });
+        google.maps.event.addDomListener(zoomOutButton, 'click', function () {
+            map.setZoom(map.getZoom() - 1);
+        });
+    }
+}
